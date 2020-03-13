a04028b69bb1686618e9079f808f45a19ce4d5f0
Arylmera <guillaume@lemer.be>
2020-03-13 09:08:21 +0100

    ajout fichier downloead.sh DNS

--- serv_dns/DNS/download.sh
+++ serv_dns/DNS/download.sh
@@ -1,8 +1,8 @@
-curl -o docker-compose.yml https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/docker-compose.yml
+curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/docker-compose.yml
 mkdir bind
-curl -o config/named.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/named.conf
-curl -o config/named.conf.default-zones https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/named.conf.default-zones
-curl -o config/named.conf.local https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/named.conf.local
-curl -o config/named.conf.options https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/named.conf.options
-curl -o config/db.internal.wt1.ephec-ti.be https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/db.internal.wt1.ephec-ti.be
-curl -o config/db.wt1.ephec-ti.be https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/db.wt1.ephec-ti.be
+curl -o bind/named.conf https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/bind/named.conf
+curl -o bind/named.conf.default-zones https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/bind/named.conf.default-zones
+curl -o bind/named.conf.local https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/bind/named.conf.local
+curl -o bind/named.conf.options https://github.com/Frodon001/2tl2AdminGr10/blob/master/serv_dns/DNS/bind/named.conf.options
+curl -o bind/db.internal.wt1.ephec-ti.be https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/bind/db.internal.wt2-10.ephec-ti.be
+curl -o bind/db.wt1.ephec-ti.be https://raw.githubusercontent.com/Frodon001/2tl2AdminGr10/master/serv_dns/DNS/bind/db.wt2-10.ephec-ti.be
