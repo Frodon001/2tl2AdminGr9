8a9e9d4c9dd312e252e02b32936fca072288f632
Florent <florent.janssens@gmail.com>
2020-03-21 16:48:33 +0100

    fix

--- serv_mail/download.sh
+++ serv_mail/download.sh
@@ -4,6 +4,7 @@ curl -o setup.sh https://raw.githubusercontent.com/tomav/docker-mailserver/maste
 
 curl -o docker-compose.yml https://raw.githubusercontent.com/tomav/docker-mailserver/master/docker-compose.yml.dist
 
-curl -o .env https://raw.githubusercontent.com/tomav/docker-mailserver/master/.env.dist
+curl -o env-mailserver https://raw.githubusercontent.com/tomav/docker-mailserver/master/env-mailserver.dist
 
-curl -o env_mailserver https://raw.githubusercontent.com/tomav/docker-mailserver/master/env-mailserver.dist
+
+curl -o env_mailServeur.env https://github.com/Frodon001/2tl2AdminGr9/blob/master/serv_mail/env_mailServeur.env.dist
