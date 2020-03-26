830d45cc6696c3238cceb089a1655d282ec40561
Arylmera <guillaume@lemer.be>
2020-03-26 12:03:17 +0100

    database flush + quick start

---
+++ database/sql-scripts/InsertData.sql
@@ -0,0 +1,6 @@
+
+insert into employees (name, last_name, department, email)
+values ('pierre','paul','IT','p.paul@wt.be');
+
+insert into produits (name, categ, stock)
+values ('cheval en bois', 'bois', 100);
