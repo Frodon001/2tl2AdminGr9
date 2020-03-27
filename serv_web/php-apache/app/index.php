de888d0e0705a1e0d9bd56700faa87296e00b636
Kotth <arnaud.nilens@gmail.com>
2020-03-27 16:00:31 +0100

    Serveur Apache-php-mysql bug

--- serv_web/php-apache/app/index.php
+++ serv_web/php-apache/app/index.php
@@ -1,2 +1,28 @@
 <?php
-echo 'Hello world!';
+$servername = "localhost";
+$username = "user";
+$password = "user1234";
+$dbname = "db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT employees, produits FROM Tables_in_db";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["employees"]."</td><td>".$row["produits"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
