a8fea914cad9b7e0cdba9d31fe84b9459324e013
Arylmera <guillaume@lemer.be>
2020-03-28 12:14:14 +0100

    docker compose DMZ

--- DMZ/download.sh
+++ DMZ/download.sh
@@ -1,3 +1,9 @@
+
+# mise a jour des images dockers
+docker pull glemer/wt_dns:latest
+docker pull httpd:2.4:latest
+docker pull glemer/wt_db:latest
+
 # download docker-compose
 curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/DMZ/docker-compose.yml
 # download DNS files
