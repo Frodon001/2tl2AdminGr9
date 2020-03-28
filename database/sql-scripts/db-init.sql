fa2ada9d117bf578d813174b11b9a87a6a0bdab3
Arylmera <guillaume@lemer.be>
2020-03-28 11:30:41 +0100

    ajout données db + docker-compose employées

--- database/sql-scripts/db-init.sql
+++ database/sql-scripts/db-init.sql
@@ -17,7 +17,17 @@ CREATE TABLE produits (
 
 
 insert into employees (name, last_name, department, email)
-values ('pierre','paul','IT','p.paul@wt.be');
+values ('quiroulle','pierre','IT','P.quiroulle@wt.be'),
+       ('dechaussure','marc','COMPTA','m.dechaussure@wt.be'),
+       ('cavil','henry','MARKET','h.cavil@wt.be'),
+       ('labrouette','bernadette','DIRECT','b.labrouette@wt.be'),
+       ('a','alex','IT','a.a@wt.be');
+
 
 insert into produits (name, categ, stock)
-values ('cheval en bois', 'bois', 100);
+values ('cheval en bois', 'bois', 100),
+       ('sculpture cheval', 'pierre', 35),
+       ('maquette cheval', 'bois', 91),
+       ('plaque', 'pierre', 10997),
+       ('plaque', 'acier', 456),
+       ('cheval en fer', 'fer', 987);
