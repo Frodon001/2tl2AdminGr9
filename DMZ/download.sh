ae6f04f3d56aaa04da9eef4f4cd02f18eb3569ea
Arylmera <guillaume@lemer.be>
2020-03-28 12:07:34 +0100

    DMZ

---
+++ DMZ/download.sh
@@ -0,0 +1,15 @@
+# download docker-compose
+
+# download DNS files
+curl -o bind/named.conf https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/named.conf
+curl -o bind/named.conf.default-zones https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/named.conf.default-zones
+curl -o bind/named.conf.local https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/named.conf.local
+curl -o bind/named.conf.options https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/named.conf.options
+curl -o bind/db.internal.wt2-9.ephec-ti.be https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/db.internal.wt2-9.ephec-ti.be
+curl -o bind/db.wt2-9.ephec-ti.be https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/db.wt2-9.ephec-ti.be
+# download Vitrine files
+curl -o public_html/index.html https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/vitrine/public_html/index.html
+# download DB files
+curl -o sql-scripts/CreateTable.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/CreateTable.sql
+curl -o sql-scripts/InsertData.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/InsertData.sql
+curl -o sql-scripts/db-init.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/db-init.sql
---
+++ DMZ/sql-scripts/db-init.sql
@@ -0,0 +1,33 @@
+CREATE TABLE employees (
+                           id int not null AUTO_INCREMENT,
+                           name varchar(25),
+                           last_name  varchar(25),
+                           department varchar(15),
+                           email  varchar(50),
+                           primary key (id)
+);
+
+CREATE TABLE produits (
+                          id int not null AUTO_INCREMENT,
+                          name  varchar(25),
+                          categ varchar(15),
+                          stock  int,
+                          primary key(id)
+);
+
+
+insert into employees (name, last_name, department, email)
+values ('quiroulle','pierre','IT','P.quiroulle@wt.be'),
+       ('dechaussure','marc','COMPTA','m.dechaussure@wt.be'),
+       ('cavil','henry','MARKET','h.cavil@wt.be'),
+       ('labrouette','bernadette','DIRECT','b.labrouette@wt.be'),
+       ('a','alex','IT','a.a@wt.be');
+
+
+insert into produits (name, categ, stock)
+values ('cheval en bois', 'bois', 100),
+       ('sculpture cheval', 'pierre', 35),
+       ('maquette cheval', 'bois', 91),
+       ('plaque', 'pierre', 10997),
+       ('plaque', 'acier', 456),
+       ('cheval en fer', 'fer', 987);
