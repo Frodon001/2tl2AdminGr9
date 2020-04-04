2ca27467387ccce7523ffccaf11fca8485388811
Arylmera <guillaume@lemer.be>
2020-04-04 11:28:38 +0200

    Download

--- Web2in1/download.sh
+++ Web2in1/download.sh
@@ -2,6 +2,10 @@
 # mise a jour des images dockers
 docker pull glemer/wt_web2in1:latest
 # download docker-compose
-curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/DMZ/docker-compose.yml
-# download DNS files
-curl -o bind/named.conf https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_dns/DNS/bind/named.conf
+curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/docker-compose.yml
+# download configs
+curl -o configs/app.conf https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/configs/app.conf
+curl -o configs/php.ini https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/configs/php.ini
+# download web files
+curl -o app/b2b/index.php https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/app/b2b/index.php
+curl -o app/vitrine/index.html https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/app/vitrine/index.html
