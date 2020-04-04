b535d507669e79ca04c8d2d619b94bbc6a9bec71
Arylmera <guillaume@lemer.be>
2020-04-04 13:54:08 +0200

    download

--- Web2in1/download.sh
+++ Web2in1/download.sh
@@ -5,6 +5,7 @@ docker pull glemer/wt_web2in1:latest
 mkdir app/b2b
 mkdir app/vitrine
 mkdir configs
+mkdir sql-scripts
 # download docker-compose
 curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/docker-compose.yml
 # download configs
@@ -13,3 +14,5 @@ curl -o configs/php.ini https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9
 # download web files
 curl -o app/b2b/index.php https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/app/b2b/index.php
 curl -o app/vitrine/index.html https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/Web2in1/app/vitrine/index.html
+# download fichiers DB
+curl -o sql-scripts/db-init.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/db-init.sql
