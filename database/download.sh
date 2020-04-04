dcc1402be8095389fdf45a6742c8ddbab1b69cf5
Arylmera <guillaume@lemer.be>
2020-04-04 12:00:10 +0200

    fix download db

--- database/download.sh
+++ database/download.sh
@@ -1,4 +1,6 @@
 curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/docker-compose.yml
+
+mkdir sql-scripts
 curl -o sql-scripts/CreateTable.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/CreateTable.sql
 curl -o sql-scripts/InsertData.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/InsertData.sql
 curl -o sql-scripts/db-init.sql https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/database/sql-scripts/db-init.sql
