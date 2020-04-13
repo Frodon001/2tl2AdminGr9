972107be73fcc8cd6d3da061f03fe40a74143a94
Arylmera <guillaume@lemer.be>
2020-04-13 11:36:32 +0200

    add readme et download.sh fix

--- VOIP/download.sh
+++ VOIP/download.sh
@@ -2,7 +2,7 @@
 docker pull glemer/wt_voip:latest
 
 #création des dossier de réception
-MKDIR conf
+mkdir conf
 
 #download docker-compose
 curl -o docker-compose.yml https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/VOIP/docker-compose.yml
