ec2573870a725d0a83cf87e1effc9ae654a6b252
Arylmera <guillaume@lemer.be>
2020-04-15 11:08:54 +0200

    Passage en docker-compose DMZ

--- OLD/serv_web/php-apache/download.sh
+++
@@ -1,6 +0,0 @@
-curl -o dockerfile https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/php-apache/dockerfile
-curl -o entrypoint.sh https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/php-apache/entrypoint.sh
-curl -o app/index.php https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/php-apache/app/index.php
-curl -o configs/app.conf https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/php-apache/configs/app.conf
-curl -o configs/php.ini https://raw.githubusercontent.com/Frodon001/2tl2AdminGr9/master/serv_web/php-apache/configs/php.ini
-
