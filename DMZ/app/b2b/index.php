0a861747549a5e0ff17c0c7241055e04f857f64a
Arylmera <guillaume@lemer.be>
2020-04-22 13:50:57 +0200

    web2in1

--- DMZ/app/b2b/index.php
+++ DMZ/app/b2b/index.php
@@ -2,13 +2,15 @@
 
 echo "Hello World";
 
-$servername = "172.16.99.3:3306";
+$servername = "172.16.99.3";
 $username = "user";
 $password = "user1234";
 $dbname = "db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect("172.16.99.3","user","user1234","db",'3306');
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
