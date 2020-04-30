5433fe2129ca071c5601790f652d3ad341795359
Kotth <arnaud.nilens@gmail.com>
2020-04-30 15:26:58 +0200

    Last php

--- DMZ/app/b2b/index.php
+++ DMZ/app/b2b/index.php
@@ -1,3 +1,31 @@
+<html>
+<head>
+    <title>Site b2b</title>
+    <style>
+        table, th {
+            border-collapse: collapse;
+            border: 1px solid;
+        }
+        td {
+            border: 1px dashed;
+        }
+    </style>
+</head>
+
+<body>
+<div id = "main">
+    <form action = "" method = "post">
+        <label>Ajouter un produit :</label>
+        <input type = "text" name = "name" id = "name" />
+        <input type = "text" name = "categ" id = "categ" />
+        <input type = "text" name = "stock" id = "stock" />
+        <br>
+        <input type = "submit" value ="Submit" name = "submit"/>
+        <br>
+    </form>
+</div>
+
+
 <?php
 
 $conn = new mysqli("172.16.99.3", "user", "user1234", "db");
@@ -7,11 +35,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+if(isset($_POST["submit"])){
+    $sql = "INSERT INTO produits (name, categ, stock) VALUES ('".$_POST["name"].",".$_POST["categ"].",".$_POST["stock"]."')";
+}
+
 $sql = "SELECT name, categ, stock FROM produits";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table style=\"border: 1px solid\"> <tr> <th>Nom</th> <th>Categorie</th> <th>Stock</th> </tr>";
+    echo "<table> <tr> <th>Nom</th> <th>Categorie</th> <th>Stock</th> </tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>".$row["name"]."</td><td>".$row["categ"]."</td><td>".$row["stock"]."</td></tr>";
@@ -22,3 +54,6 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
+</body>
+</html>
