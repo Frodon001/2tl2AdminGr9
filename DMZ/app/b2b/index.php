5dd8267f0b59bd6b53de75dfba3ab3ec61ef97f3
Kotth <arnaud.nilens@gmail.com>
2020-05-01 18:46:04 +0200

    up

--- DMZ/app/b2b/index.php
+++ DMZ/app/b2b/index.php
@@ -43,7 +43,7 @@ if(isset($_POST["submit"])){
     if($conn->query($sql)===TRUE){
         echo "Table mise à jour.";
     } else {
-        echo "Erreur : ". $sql . "<br>" . $conn->error;
+        echo "Erreur: " . $sql . "<br>" . $conn->error;
     }
 }
 
