3e3136d30fc17887c833d1ea42df5d88a5ed5e72
Kotth <arnaud.nilens@gmail.com>
2020-05-01 18:40:07 +0200

    Debugging PHP

--- DMZ/app/b2b/index.php
+++ DMZ/app/b2b/index.php
@@ -45,6 +45,20 @@ if(isset($_POST["submit"])){
     } else {
         echo "Erreur";
     }
+
+    $sql = "SELECT name, categ, stock FROM produits";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table> <tr> <th>Nom</th> <th>Categorie</th> <th>Stock</th> </tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["name"]."</td><td>".$row["categ"]."</td><td>".$row["stock"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
 }
 
 $sql = "SELECT name, categ, stock FROM produits";
@@ -60,24 +74,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-$conn->close();
-?>
-
-<?php
-
-$conn = new mysqli("172.16.99.3", "user", "user1234", "db");
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if(isset($_POST["submit"])){
-    $sql = "INSERT INTO produits (name, categ, stock) VALUES ('".$_POST["name"].",".$_POST["categ"].",".$_POST["stock"]."')";
-}
-
-
-
 $conn->close();
 ?>
 
